c5ee33c577db166fb11af3c07c1f59d87a58df7b
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-26 13:20:05 -0600

    import msw conditionally (only for prod) in App component

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from '../../pages/Home';
+if (process.env.NODE_ENV === 'development') {
+
+  const { worker } = require('../../mocks/browser')
+
+  worker.start()
+
+}
+
 
 function App() {
   return (
