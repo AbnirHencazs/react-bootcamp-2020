f785d3992780535c13d0072ee818abe6b21feece
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-03-29 11:47:39 -0600

    test VideoCardList renders videos obtained from msw

--- src/components/VideoCardList/index.test.js
+++ src/components/VideoCardList/index.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import {items} from '../../youtube-videos-mock';
+import { server } from '../../mocks/server';
 import VideoCardList from './index';
 
 test('render video card list component', () => {
@@ -18,3 +19,22 @@ test('renders a list of VideoCards', () => {
 
   expect(VideoCards.length).toBeGreaterThanOrEqual(24)
 })
+
+describe("VideoCardList component", () => {
+  beforeAll( () => server.listen() )
+  afterEach( () => server.resetHandlers() )
+  afterAll( () => server.close() )
+
+  test("Obtain videos from msw and render them in VideoCardList component", async () => {
+    const response = await fetch("/videos")
+    const data = await response.json()
+
+    render(<VideoCardList videos={data.mockData.items} channel={data.mockData.items[0]}/>)
+    const VideoCardListComponent = screen.getByTestId("VideoCardList")
+
+    expect( VideoCardListComponent ).toBeInTheDocument
+
+    const VideoCards = screen.getAllByRole("article")
+    expect( VideoCards.length ).toBeGreaterThanOrEqual(24)
+  })
+}) 
