bcb504a87b9334bf46401b48e6d0b126217eeec8
mr-m33s33ks <briansanchezizq@gmail.com>
2021-03-30 00:27:15 -0600

    Use custom hook useGapi

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from '../../components/Navbar';
 import VideoCardList from '../../components/VideoCardList';
 import useGapi from '../../hooks/useGapi';
 
 function HomePage() {
-  const {videos, getVideos} = useGapi("/videos")
+  
+  const {videos, getVideos, client} = useGapi('')
 
   useEffect( () => {
+    if( client === undefined ) return;
     getVideos()
-  }, [])
+  }, [client])
 
   return (
     <>
