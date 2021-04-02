6f3a5f5d0a7021bc8eb617996d5bd6608a7a4ee5
Brian Sanchez <briansanchezizq@gmail.com>
2021-04-02 11:53:48 -0600

    VideoPlayer component to display specific video; todo videoinfo

--- src/components/VideoPlayer/index.jsx
+++ src/components/VideoPlayer/index.jsx
@@ -5,7 +5,7 @@ const VideoPlayer = () =>{
     const { videoId } = useParams();
     return(
         
-        <div className="flex w-8/12 h-4/5 px-10">
+        <div className="flex w-8/12 h-4/5 px-10 ">
             <iframe className="w-full h-5/5" src={`https://www.youtube.com/embed/${videoId}`} title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
         
