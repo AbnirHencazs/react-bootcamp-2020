e1d13d6c341174673ee1be6e9f93d5fe33b15ddf
Brian Sanchez <briansanchezizq@gmail.com>
2021-04-02 19:32:57 -0600

    implement search feature in VideoPage

--- src/pages/Video/Video.page.jsx
+++ src/pages/Video/Video.page.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 import VideoPlayer from '../../components/VideoPlayer';
 import RelatedVideos from '../../components/RelatedVideos'
 import Navbar from '../../components/Navbar';
+import { useHistory } from 'react-router-dom';
 
 const VideoPage = () => {
-    
+    const history = useHistory()
+    const updateSearch = (childData) =>{
+        history.push(`/q:${childData}`)
+    }
     return(
         <>
-            <Navbar/>
+            <Navbar
+                callBack={updateSearch}/>
             <div className="flex h-screen mt-5">
                 <div className="flex flex-column w-full h-4/5">
                     <VideoPlayer
