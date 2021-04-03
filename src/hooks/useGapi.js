7992f10945edde795d63f677761c2bd266beedc2
Brian Sanchez <briansanchezizq@gmail.com>
2021-04-02 18:35:21 -0600

    refactor useGapi to retrieve a single video info

--- src/hooks/useGapi.js
+++ src/hooks/useGapi.js
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
-const useGapi = (search = "", relatedTo = "") => {
+const useGapi = (search = "", relatedTo = "", isList = true) => {
 
     const [ videos, setVideos ] = useState([])
+    const [ video, setVideo ] = useState(undefined)
     const [ isLoading, setIsLoading ] = useState(false)
     const { client, load } = window.gapi;
 
     useEffect( () => {
         if( client === undefined ) return;
         initGapi()
-    }, [ search, client] )
+    }, [ search, client, relatedTo, isList] )
 
     const loadGapi = () => new Promise( (resolve,reject) => {
 
@@ -35,25 +36,38 @@ const useGapi = (search = "", relatedTo = "") => {
         }
     }
     
-    const getVideos = async () => {
-        
-        const response = await client.request({
-            path: `https://www.googleapis.com/youtube/v3/search`,
-            params: {
-                part:'snippet',
-                maxResults:25,
-                q:search,
-                relatedToVideoId: relatedTo,
-                type:"video",
-            }
-        })
-        const data = response.result.items
-        setVideos(data)
-        setIsLoading(false)
-        console.log(data)
-    }
+    const getVideos = useCallback( async () => {
+        if(isList){
+            const response = await client.request({
+                path: `https://www.googleapis.com/youtube/v3/search`,
+                params: {
+                    part:'snippet',
+                    maxResults:25,
+                    q:search,
+                    relatedToVideoId: relatedTo,
+                    type:"video",
+                }
+            })
+            const data = response.result.items
+            setVideos(data)
+            setIsLoading(false)
+            console.log(data)
+        }else{
+            const response = await client.request({
+                path:`https://www.googleapis.com/youtube/v3/videos`,
+                params:{
+                    part: "snippet",
+                    id: relatedTo
+                }
+            })
+            const data = response.result.items[0]
+            setVideo(data)
+            setIsLoading(false)
+            console.log(data)
+        }
+    }, [search, relatedTo, isList] )
 
-    return { videos, isLoading }
+    return { videos, video, isLoading }
 }
 
 export default useGapi;
