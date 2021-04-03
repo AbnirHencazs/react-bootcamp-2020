4514d04eebd9c200b7792a1c5a59ed9655dcc48b
Brian Sanchez <briansanchezizq@gmail.com>
2021-04-02 21:38:52 -0600

    refactor searchForm test snapshot to match SearchForm component

--- src/components/SearchForm/index.test.js
+++ src/components/SearchForm/index.test.js
@@ -13,6 +13,7 @@ describe( 'SearchForm', () => {
               class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"
               placeholder="Search"
               type="text"
+              value=""
             />
         `)
     })
