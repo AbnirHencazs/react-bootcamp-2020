3c74da176cafdd64d48b3e5f70d41db181e222e4
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-04-13 08:56:37 -0500

    redefine exact path for searchQuery in App component

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import HomePage from '../../pages/Home';
 import VideoPage from '../../pages/Video'
-import RelatedVideos from '../RelatedVideos';
 import VideoPlayer from '../VideoPlayer';
 
 if (process.env.NODE_ENV === 'development') {
@@ -22,7 +21,7 @@ function App() {
         <Route exact path="/">
           <HomePage />
         </Route>
-        <Route exact path="/q:searchQuery">
+        <Route exact path="/q/:searchQuery">
           <HomePage/>
         </Route>
         <Route exact path="/video/:videoId" component={VideoPlayer}>
