cc189a3ed5353d27a783dd14b3fa49cf3c63046f
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-04-13 11:52:30 -0500

    implemntation of searchQuery from GlobalContext in HomePage; deprecate useParams

--- src/pages/Home/Home.page.jsx
+++ src/pages/Home/Home.page.jsx
@@ -3,10 +3,11 @@ import NavBar from '../../components/Navbar';
 import VideoCardList from '../../components/VideoCardList';
 import useGapi from '../../hooks/useGapi';
 import { useParams } from 'react-router-dom';
+import { useGlobals } from '../../state/GlobalProvider';
 
 function HomePage() {
   
-  const { searchQuery } = useParams();
+  const { searchQuery } = useGlobals();
   const { videos } = useGapi( searchQuery ? searchQuery : "", null)
 
   return (
