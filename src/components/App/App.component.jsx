dcfe91f695c3791c75a1eb49b958ebcd738a3e29
mr-m33s33ks <briansanchezizq@gmail.com>
2021-04-20 20:43:21 -0500

    Import Login and 404 pages and give them routes

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
 import HomePage from '../../pages/Home';
 import VideoPage from '../../pages/Video'
 import VideoPlayer from '../VideoPlayer';
+import NotFound from '../../pages/NotFound';
+import LoginPage from '../../pages/Login';
 import GlobalProvider from '../../state/GlobalProvider'
 
 if (process.env.NODE_ENV === 'development') {
@@ -25,6 +27,12 @@ function App() {
           </Route>
           <Route exact path="/video/:videoId" component={VideoPlayer}>
             <VideoPage/>
+          </Route>
+          <Route>
+            <LoginPage/>
+          </Route>
+          <Route path="*"> 
+            <NotFound />
           </Route>
         </Switch>
       </BrowserRouter>
