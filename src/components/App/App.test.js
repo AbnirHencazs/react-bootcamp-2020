29d12b9e276a2ff8d10b0d0e750891eef7e45b64
mr-m33s33ks <briansanchezizq@gmail.com>
2021-04-20 20:37:06 -0500

    add app test to check if Login page, 404 page, favourites page and favourites detail page renders

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,6 +3,8 @@ import { screen, render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import App from './index'
 import mockData from '../../youtube-videos-mock'
+import LoginPage from '../../pages/Login';
+import NotFound from '../../pages/NotFound';
 
 describe("App component", () => {
     beforeAll( () => {
@@ -32,5 +34,70 @@ describe("App component", () => {
         
         expect( VideoCardListComponent ).toBeInTheDocument()
         expect( NavbarComponent ).toBeInTheDocument()
+    })
+    test("renders Login Page", () => {
+        render(
+            <MemoryRouter
+                initialEntries={["/login"]}>
+                <LoginPage/>
+            </MemoryRouter>
+        )
+        const heading = screen.getByRole("heading",{
+            name: /Ingresa a tu cuenta/i
+        })
+        const loginForm = screen.getByRole("form")
+        const loginButton = screen.getByRole("button",{
+            name: /Ingresar/i
+        })
+
+        expect(heading).toBeInTheDocument()
+        expect(loginForm).toBeInTheDocument()
+        expect(loginButton).toBeInTheDocument()
+    })
+    test("Renders Favourites page", ()=>{
+        render(
+            <MemoryRouter
+                initialEntries={["favourites"]}
+                initialIndex={1}>
+                <App/>
+            </MemoryRouter>
+        )
+
+        const FavouriteVideosList = screen.getAllByRole("article")
+
+        expect(FavouriteVideosList.length).toBeGreaterThanOrEqual(0)
+    })
+    test("renders favourite video detail page", () => {
+        render(
+            <MemoryRouter
+                initialEntries={[`/favourites/${mockData.items[1].id.videoId}`]}
+                initialIndex={1}>
+                <App/>
+            </MemoryRouter>
+        )
+        
+        const VideoTitle = screen.getByRole("heading")
+        const VideoDescription = screen.getByRole("paragraph")
+        const FavouriteVideosList = screen.getAllByRole("article")
+
+        expect(VideoTitle).toBeInTheDocument()
+        expect(VideoTitle).toHaveClass("")
+        expect(VideoDescription).toBeInTheDocument()
+        expect(VideoDescription).toHaveClass()
+        expect(FavouriteVideosList.length).toBeGreaterThanOrEqual(1)
+    })
+    test("renders 404 page", () => {
+        render(
+            <MemoryRouter
+                initialEntries={["*"]}>
+                <NotFound/>
+            </MemoryRouter>
+        )
+
+        const NotFoundTitle = screen.getByRole("heading", {
+            name: /Not all who wander are lost, but i'm afraid you are/i
+        })
+
+        expect(NotFoundTitle).toBeInTheDocument()
     })
 })
