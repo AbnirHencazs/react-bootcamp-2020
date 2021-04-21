8de0483850ad0d201f2273fd2aaf04f678a6c135
mr-m33s33ks <briansanchezizq@gmail.com>
2021-04-21 16:42:20 -0500

    test reducer action for user updating

--- src/state/Reducer.test.js
+++ src/state/Reducer.test.js
@@ -1,10 +1,15 @@
 import reducer from './globalReducer';
-import { SET_SEARCH_QUERY, SET_THEME } from '../utils/constants';
+import { SET_SEARCH_QUERY, SET_THEME, SET_USER } from '../utils/constants';
 
 describe('reducer', () => {
   const iniState = {
     theme: 'light',
     searchQuery: '',
+    user:{
+      id: '',
+      name: '',
+      avatarUrl: ''
+    }
   };
 
   test('set theme action works properly', () => {
@@ -16,6 +21,11 @@ describe('reducer', () => {
     const expectedState = {
       theme: 'dark',
       searchQuery: '',
+      user:{
+        id: '',
+        name: '',
+        avatarUrl: ''
+      }
     };
 
     expect(reducer(iniState, action)).toEqual(expectedState);
@@ -30,6 +40,11 @@ describe('reducer', () => {
     const expectedState = {
       theme: 'dark',
       searchQuery: '',
+      user:{
+        id: '',
+        name: '',
+        avatarUrl: ''
+      }
     };
 
     expect(reducer(iniState, action)).toEqual(expectedState);
@@ -44,8 +59,36 @@ describe('reducer', () => {
     const expectedState = {
       theme: 'light',
       searchQuery: 'wizeline',
+      user:{
+        id: '',
+        name: '',
+        avatarUrl: ''
+      }
     };
 
     expect(reducer(iniState, action)).toEqual(expectedState);
   });
+
+  test("Set user works action works correctly", () => {
+    const action = {
+      type: SET_USER,
+      payload: {
+        id: '123',
+        name: 'Wizeline',
+        avatarUrl: 'https://media.glassdoor.com/sqll/868055/wizeline-squarelogo-1473976610815.png'
+      }
+    }
+
+    const expectedState = {
+      theme: 'light',
+      searchQuery: '',
+      user:{
+        id: '123',
+        name: 'Wizeline',
+        avatarUrl: 'https://media.glassdoor.com/sqll/868055/wizeline-squarelogo-1473976610815.png'
+      }
+    }
+
+    expect(reducer(iniState, action)).toEqual(expectedState)
+  })
 });
