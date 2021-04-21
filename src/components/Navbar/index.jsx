448b41f26e45008b554a8a98ef8d4d2f2e8683ec
mr-m33s33ks <briansanchezizq@gmail.com>
2021-04-21 18:13:49 -0500

    implement dropdown menu to handle login and logout functionality

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import { useGlobals } from '../../state/GlobalProvider';
 import SearchForm from '../SearchForm';
 
 const Navbar = () => {
 
-  const { toggleTheme } = useGlobals();
-
+  const { toggleTheme, user, updateUser } = useGlobals();
+  const [dropDown, setDropDown] = useState(false)
+  
+  const handleLogout = () => {
+    updateUser()
+    setDropDown(false)
+  }
   return (
     <>
       <nav className="flex flex-column w-100 justify-center md:justify-between px-5 py-5 bg-blue-300 dark:bg-indigo-900 dark:bg-opacity-60">
@@ -47,23 +53,54 @@ const Navbar = () => {
               />
             </svg>
           </div>
-          <div className="m-auto border-2 rounded-full border-white bg-white hover:bg-blue-200">
-            <svg
-              className="flex w-6 md:w-10"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={1}
-                d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-              />
-            </svg>
+          <div onClick={() => {setDropDown(!dropDown)}} className="m-auto border-2 rounded-full border-white cursor-pointer bg-white hover:bg-blue-200">
+            {
+              user.authenticated ?
+              <img className="rounded-full h-10 w-10 cursor-pointer" src={user.avatarUrl} alt="user avatar"/>
+              :
+              <svg
+                className="flex w-6 md:w-10"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={1}
+                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
+            }
           </div>
         </div>
+        {
+          dropDown ?
+          <div className="fixed right-0 mt-5 mr-20 bg-white shadow rounded">
+            {
+              (user.authenticated) ?
+              <div className="dropdown inline-block relative">
+                <div className=" py-2 px-4 rounded inline-flex items-center">
+                  {`Welcome, ${user.name}`}
+                </div>
+                <ul className="dropdown-menu absolute  text-gray-700 pt-1">
+                  <li className=""><button to="/login" onClick={handleLogout} className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Log out</button></li>
+                </ul>
+              </div>
+              : 
+              <div className="dropdown inline-block relative">
+                <div className=" py-2 px-4 rounded inline-flex items-center">
+                  You're not logged in
+                </div>
+                <ul className="dropdown-menu absolute  text-gray-700 pt-1">
+                  <li className=""><Link to="/login" className="rounded-t bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap" href="#">Log in</Link></li>
+                </ul>
+              </div>
+            }
+          </div>
+          :<div className="hidden"></div>
+        }
       </nav>
     </>
   );
