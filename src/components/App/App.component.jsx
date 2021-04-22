b55f9e221f35d6055a03684873c5f3de7d7f54d2
mr-m33s33ks <briansanchezizq@gmail.com>
2021-04-21 19:04:10 -0500

    fix login Route

--- src/components/App/App.component.jsx
+++ src/components/App/App.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, Routes } from 'react-router-dom';
+import Protected from '../Protected'
 import HomePage from '../../pages/Home';
 import VideoPage from '../../pages/Video'
 import VideoPlayer from '../VideoPlayer';
 import NotFound from '../../pages/NotFound';
 import LoginPage from '../../pages/Login';
+import FavouritePage from '../../pages/Favourite';
 import GlobalProvider from '../../state/GlobalProvider'
 
 if (process.env.NODE_ENV === 'development') {
@@ -28,7 +30,8 @@ function App() {
           <Route exact path="/video/:videoId" component={VideoPlayer}>
             <VideoPage/>
           </Route>
-          <Route>
+          <Protected path="/favourites" component={FavouritePage}/>
+          <Route exact path="/login">
             <LoginPage/>
           </Route>
           <Route path="*"> 
