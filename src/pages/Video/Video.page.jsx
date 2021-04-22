0c5908ea11bba343b4440f561b3e5ac638f779bc
mr-m33s33ks <briansanchezizq@gmail.com>
2021-04-22 03:50:47 -0500

    refactor VideoPage to obtain videoInfo and relatedVideos trhough useGapi, pass them as props

--- src/pages/Video/Video.page.jsx
+++ src/pages/Video/Video.page.jsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import VideoPlayer from '../../components/VideoPlayer';
+import { useParams } from 'react-router-dom'
+import useGapi from '../../hooks/useGapi'
 import RelatedVideos from '../../components/RelatedVideos'
 import Navbar from '../../components/Navbar';
 
 const VideoPage = () => {
-    
+    const { videoId } = useParams()
+    const { video, isLoading } = useGapi("", videoId, false)
+    const { videos } = useGapi("", videoId)
     return(
         <>
             <Navbar/>
             <div className="flex h-screen pt-5 dark:bg-gray-800">
                 <div className="flex flex-col md:flex-row w-full h-4/5">
                     <VideoPlayer
+                        isLoading={isLoading}
+                        video={video}
+                        videoId={videoId}
                         />
                     <RelatedVideos
-                        />
+                        isLoading={isLoading}
+                        videos={videos}/>
                 </div>
             </div>
         </>
