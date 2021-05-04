55e790d1b9a493e43626888ae0a8378eeffb5d45
mr-m33s33ks <briansanchezizq@gmail.com>
2021-05-03 19:16:05 -0500

    add aria label to form in SearchForm component

--- src/components/SearchForm/index.jsx
+++ src/components/SearchForm/index.jsx
@@ -17,7 +17,9 @@ const SearchForm = () => {
     }
 
     return(
-        <form onSubmit={handleSubmit}>
+        <form
+            aria-label="form" 
+            onSubmit={handleSubmit}>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:ring focus:ring-blue-500 focus:ring-opacity-50"
               type="text"
