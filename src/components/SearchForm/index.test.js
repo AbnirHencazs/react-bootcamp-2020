9d8a6d2eff6a3cf7cae1c6e0cc1e85b6dc1966d1
mr-m33s33ks <briansanchezizq@gmail.com>
2021-05-03 19:15:19 -0500

    test submitSearchQuery

--- src/components/SearchForm/index.test.js
+++ src/components/SearchForm/index.test.js
@@ -1,9 +1,26 @@
 import React from 'react'
-import { screen, render } from '@testing-library/react';
+import { screen, render, fireEvent } from '@testing-library/react';
+import { renderHook } from '@testing-library/react-hooks'
 import SearchForm from './index';
 import userEvent from '@testing-library/user-event';
+import GlobalProvider, { useGlobals } from '../../state/GlobalProvider';
 
+const submitSearchQuery = jest.fn()
+jest.mock('../../state/GlobalProvider', () => {
+    return({
+        __esModule: true, // this property makes it work
+        ...jest.requireActual('../../state/GlobalProvider'),
+        useGlobals: jest.fn()
+    })
+})
 describe( 'SearchForm', () => {
+    beforeAll( () => {
+        
+        useGlobals.mockReturnValue( {
+            submitSearchQuery
+        } )
+    })
+    
     test("render initially", () => {
         render( <SearchForm/> )
         const searchInput = screen.getByPlaceholderText("Search")
@@ -25,5 +42,19 @@ describe( 'SearchForm', () => {
         const searchInput = screen.getByPlaceholderText(/Search/i)
         userEvent.type(searchInput, 'Wizeline')
         expect(searchInput).toHaveValue('Wizeline')
+    })
+
+    test("test submit state changed", () => {
+        const { submitSearchQuery } = useGlobals()
+        
+        render(
+            <GlobalProvider>
+                <SearchForm/>
+            </GlobalProvider>
+        )
+        const input = screen.getByPlaceholderText(/Search/i)
+        userEvent.type(input, "Wizeline")
+        fireEvent.submit(screen.getByRole("form"))
+        expect(submitSearchQuery).toHaveBeenCalledWith("Wizeline")
     })
 } );
